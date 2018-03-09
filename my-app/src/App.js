18df5b437ce857370ea694b8f92242aab4649ed0
FernandoRogelin <nando_rogelin@hotmail.com>
2018-03-08 23:17:59 -0300

    layout of todo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class todoList extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="todo-list">
+        <div>
+          <p className="title">Todo-List with ReactJS</p>
+        </div>
+        <div className="todo-item">
+          <input type="text"></input>
+          <button className="addItem">Add Item</button>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default todoList;
