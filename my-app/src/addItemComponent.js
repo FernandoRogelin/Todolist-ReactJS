62c79ed5207e8426126c36a8d0a88903f02f0a5f
Fernando Rogelin <fernando.filho@ilegra.com>
2018-03-14 09:29:02 -0300

    adicionado uma div quando colocar algum item

--- my-app/src/addItemComponent.js
+++ my-app/src/addItemComponent.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class addItem extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.palavras = []
+    this.palavras = [];
     this.state = {
-      texto: '',
+      texto: "",
       cont: 0
-    }
+    };
   }
 
   onClicou = () => {
-    this.palavras[this.state.cont] = this.state.texto;
+    this.palavras[this.state.cont] = <div>{this.state.texto}</div>;
     console.log(this.palavras);
     this.setState({
       cont: this.state.cont + 1
     });
-  }
+  };
 
-  render(){
+  render() {
     return (
       <div className="todo-item">
-        <input onInput={(e) => this.setState({texto: e.target.value})} name="text" type="text"></input>
-        <button className="addItem" onClick={this.onClicou}>Add Item</button>
+        <input
+          onInput={e => this.setState({ texto: e.target.value })}
+          name="text"
+          type="text"
+        />
+        <button className="addItem" onClick={this.onClicou}>
+          Add Item
+        </button>
         <div className="itens">{this.palavras}</div>
       </div>
     );
