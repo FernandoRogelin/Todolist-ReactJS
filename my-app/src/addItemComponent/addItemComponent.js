940afc85196485f2dcbb71e7c2bc7975a9f25229
Fernando Rogelin <fernando.filho@ilegra.com>
2018-03-28 17:22:26 -0300

    Arrumando o code

--- my-app/src/addItemComponent/addItemComponent.js
+++ my-app/src/addItemComponent/addItemComponent.js
@@ -4,14 +4,26 @@ import "./addItem.css";
 export default class addItem extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      palavras: [],
+      texto: ""
+    };
   }
+
+  Clicou = () => {
+    console.log(this.state.texto);
+    this.setState({
+      palavras: this.state.palavras.push(this.state.texto)
+    });
+  };
 
   render() {
     return (
       <div className="todo-item">
-        <input />
-        <button className="addItem">Add Item</button>
+        <input onInput={e => this.setState({ texto: e.target.value })} />
+        <button className="addItem" onClick={this.Clicou}>
+          Add Item
+        </button>
         <div className="itens" />
       </div>
     );
