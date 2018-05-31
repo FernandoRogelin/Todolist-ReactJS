73cff5f72ded044c43c44a4a5907b2c5d7d9dae6
FernandoRogelin <nando_rogelin@hotmail.com>
2018-05-31 01:25:28 -0300

    Adicionado funcionalidade de adicionar item

--- my-app/src/addItemComponent/addItemComponent.js
+++ my-app/src/addItemComponent/addItemComponent.js
@@ -2,30 +2,40 @@ import React, { Component } from "react";
 import "./addItem.css";
 
 export default class addItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      palavras: [],
-      texto: ""
-    };
-  }
+  state = {
+    palavras: [],
+    texto: '',
+  };
 
-  click = () => {
-    this.state.palavras.push(this.state.texto);
+  AddItem = () => {
+    var newArray = this.state.palavras.slice();
+    newArray.push(this.state.texto);
+    this.setState({ palavras: newArray });
+  };
+
+  onChangeText = evt => {
+    this.state.texto = evt.target.value;
+  };
+
+  removePeople = evt => {
+    var array = [...this.state.palavras];
+    var index = array.indexOf(evt.target.value);
+    array.splice(index, 1);
+    this.setState({ palavras: array });
   };
 
   render() {
     const listItems = this.state.palavras.map(frases => (
       <div className="itens">
         {frases}
-        <button className="excluir">X</button>
+        <button className="excluir" onClick={this.removePeople}>X</button>
       </div>
     ));
 
     return (
       <div className="todo-item">
-        <input onInput={e => this.setState({ texto: e.target.value })} />
-        <button className="addItem" onClick={this.click}>
+        <input onChange={this.onChangeText} />
+        <button className="addItem" onClick={this.AddItem}>
           Add Item
         </button>
         {listItems}
